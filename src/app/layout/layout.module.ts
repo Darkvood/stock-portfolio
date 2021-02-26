17781fbeddde8b4cb4e8df559edc44ba8035ea2d
Danil Katuhin <the.darkvood@mail.ru>
2021-02-27 00:20:16 +0300

    feat: Добавлен meterial.module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+
 import { DefaultComponent } from "./default/default.component";
 import { AppRoutingModule } from "../app-routing.module";
+import { MaterialModule } from "../material/material.module";
 
 @NgModule({
   declarations: [DefaultComponent],
   exports: [DefaultComponent],
-  imports: [CommonModule, AppRoutingModule]
+  imports: [CommonModule, AppRoutingModule, MaterialModule]
 })
 export class LayoutModule {}
