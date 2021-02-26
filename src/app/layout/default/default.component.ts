8af831e1c3b0305b7bcd7379ff69d1e4e1b320d0
Danil Katuhin <the.darkvood@mail.ru>
2021-02-27 02:40:37 +0300

    feat(layout): Адаптив sidebar

--- src/app/layout/default/default.component.ts
+++ src/app/layout/default/default.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from "@angular/core";
+import { BreakpointObserver, Breakpoints, BreakpointState } from "@angular/cdk/layout";
 
 @Component({
   selector: "app-layout-default",
   templateUrl: "./default.component.html",
   styleUrls: ["./default.component.scss"]
 })
-export class DefaultComponent {
-  sideBarOpen = true;
+export class DefaultComponent implements OnInit {
+  sidebar = {
+    active: true,
+    toggleable: false
+  };
 
-  toggleSideBar(): void {
-    this.sideBarOpen = !this.sideBarOpen;
+  constructor(public breakpointObserver: BreakpointObserver) {
+    this.changeSidebarModeIfNeeded = this.changeSidebarModeIfNeeded.bind(this);
+  }
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small]).subscribe(this.changeSidebarModeIfNeeded);
+  }
+
+  toggleSidebar(): void {
+    this.sidebar.active = !this.sidebar.active;
+  }
+
+  changeSidebarModeIfNeeded(state: BreakpointState): void {
+    const forceOpen = !state.matches;
+
+    if (forceOpen) {
+      this.sidebar = { active: true, toggleable: false };
+    } else {
+      this.sidebar = { active: false, toggleable: true };
+    }
   }
 }
--- src/app/layout/default/header/header.component.ts
+++ src/app/layout/default/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
   selector: "app-header",
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
   styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent {
-  constructor() {}
+  @Input() menuToggleable: string;
+  @Output() toggleAside: EventEmitter<void> = new EventEmitter();
+
+  toggleSideBar(): void {
+    this.toggleAside.emit();
+  }
 }
