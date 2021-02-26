bcc28d31071ccdaaee44c19ba448cb401585964e
Danil Katuhin <the.darkvood@mail.ru>
2021-02-27 01:35:00 +0300

    feat(layout/aside): Добавлен компонент навигации

---
+++ src/app/layout/default/aside/aside.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-aside",
+  templateUrl: "./aside.component.html"
+})
+export class AsideComponent {
+  constructor() {}
+}
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,10 +5,11 @@ import { DefaultComponent } from "./default/default.component";
 import { AppRoutingModule } from "../app-routing.module";
 import { MaterialModule } from "../material/material.module";
 import { HeaderComponent } from "./default/header/header.component";
+import { AsideComponent } from "./default/aside/aside.component";
 
 @NgModule({
   imports: [CommonModule, AppRoutingModule, MaterialModule],
-  declarations: [DefaultComponent, HeaderComponent],
+  declarations: [DefaultComponent, HeaderComponent, AsideComponent],
   exports: [DefaultComponent]
 })
 export class LayoutModule {}
