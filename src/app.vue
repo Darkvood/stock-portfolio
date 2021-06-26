b73d75525decc4d6a7f8502844b24f9292fe9aa2
Danil Katuhin <the.darkvood@mail.ru>
2021-06-26 20:37:11 +0300

    Удалены артифакты

--- src/app.vue
+++ src/app.vue
@@ -1,26 +1,3 @@
 <template>
-  <div>
-    app template
-
-    <div v-for="n in 10">
-      {{ n }}
-    </div>
-
-    <div v-html="foo"></div>
-  </div>
+  <div>app</div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      foo: "foo"
-    };
-  },
-  mounted() {
-    const foo2 = "das";
-
-    console.log("Foo mounted");
-  }
-};
-</script>
