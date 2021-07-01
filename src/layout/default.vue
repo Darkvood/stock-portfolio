0e1841351ed9586053e06dc2e15ba8dab2ddab2a
Danil Katuhin <the.darkvood@mail.ru>
2021-07-01 18:08:48 +0300

    app: Установлен router, vuex

---
+++ src/layout/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>Stock portfolio</header>
+
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<script>
+export default {
+  name: "LayoutDefault"
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from "vue";
-import App from "~/app.vue";
+import { createApp, h } from "vue";
+
+import { store } from "~/store";
+import { router } from "~/router";
 
 import "~/assets/styles/main.scss";
 
-createApp(App).mount("#app");
+import Layout from "~/layout/default.vue";
+
+const app = createApp(Layout);
+
+app.use(store);
+app.use(router);
+
+app.mount("#app");
