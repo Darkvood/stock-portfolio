0c530a8b0e32c1c17b2393414682181013395d88
Danil Katuhin <the.darkvood@mail.ru>
2021-07-03 16:54:41 +0300

    test: Удалены инструменты тестирования vue

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,12 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-  moduleFileExtensions: ["js", "json", "vue"],
+  moduleFileExtensions: ["js"],
   testMatch: ["<rootDir>/test/**/*.js"],
   moduleNameMapper: {
     "^~/(.*)$": "<rootDir>/src/$1"
   },
   transform: {
-    ".*\\.(vue)$": "vue-jest",
     ".*\\.(js)$": "babel-jest"
-  },
-  globals: {
-    "vue-jest": {
-      babelConfig: true
-    }
   },
   collectCoverage: false
 };
